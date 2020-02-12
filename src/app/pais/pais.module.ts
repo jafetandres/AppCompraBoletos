9c937a1d131d2b4e822ac4a9eceaba8338988a50
jafetandres <46006892+jafetandres@users.noreply.github.com>
2020-02-12 12:43:16 -0500

    pais cambios

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -5,8 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { PaisPageRoutingModule } from './pais-routing.module';
-
+import { Routes, RouterModule } from '@angular/router';
 import { PaisPage } from './pais.page';
+import { PaisResolver } from './pais.resolver';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PaisPage,
+    resolve: {
+      data: PaisResolver
+    }
+  }
+];
 
 @NgModule({
   imports: [
@@ -16,5 +27,6 @@ import { PaisPage } from './pais.page';
     PaisPageRoutingModule
   ],
   declarations: [PaisPage]
+
 })
 export class PaisPageModule {}
